238a8ca1f4179400d03af29b62d4bb97779cd80c
Chris Phillips <cminion@gmail.com>
2024-07-26 13:07:49 +0100

    created config for all students

--- datapower/run.sh
+++ datapower/run.sh
@@ -1,5 +1,6 @@
 #helper script
 for i in $(ls -d App*) ; do
+  cp AppDomain01/config/AppDomain01.cfg.json $i/config/$i.cfg.json
   no=$(expr 9443 + $(echo $i | tr -d '[a-z][A-Z]'))
-  gsed -i s/...change_HTTPSSourceProtocolHandler_fsh-https-gitops_LocalPort.../$no/ $i/config/$i.cfg.json
+  gsed -i s/9444/$no/ $i/config/$i.cfg.json
 done
