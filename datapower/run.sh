6447db0bc3aeac8e7131279cf327fc598464206e
Chris Phillips <cminion@gmail.com>
2024-07-26 15:08:59 +0100

    added gw to all domains

--- datapower/run.sh
+++ datapower/run.sh
@@ -1,6 +1,10 @@
 #helper script
 for i in $(ls -d App*) ; do
-  cp AppDomain01/config/AppDomain01.cfg.json $i/config/$i.cfg.json
+  cp AppDomain05/config/AppDomain05.cfg.json $i/config/$i.cfg.json
+  cp -r AppDomain05/local $i/
   no=$(expr 9443 + $(echo $i | tr -d '[a-z][A-Z]'))
-  gsed -i s/9444/$no/ $i/config/$i.cfg.json
+  gsed -i s/9448/$no/ $i/config/$i.cfg.json
+
+  gsed -i s/local....js/local\:\\\/\\\/\\\/gws.js/ $i/config/$i.cfg.json
+
 done
